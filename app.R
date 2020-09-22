5b1de529292763e5d23796e27d6b53682954c2a6
Freddy Drennan <drennanfreddy@gmail.com>
2020-09-21 21:50:57 -0600

    update;

--- app.R
+++ app.R
@@ -5,38 +5,48 @@ library(readr)
 library(purrr)
 
 #' withConsoleRedirect
+# renv::install('fdrennan/ndexrstrator', rebuild = TRUE)
 library(ndexie)
 
 # Example usage
 
 ui <- fluidPage(
+    textInput('text_1',
+              'TEXT INPUT',
+              value = "0",
+              width = '100%',
+              placeholder = 'An input of Text'),
+    actionButton(inputId = "do", label =  "Click Me", width = '100%'),
     pre(id = "console")
 )
 
 server <- function(input, output, session) {
     observe({
         # invalidateLater(5000)
-
-        withConsoleRedirect("console", {
-            {
-                print('Library Paths')
-                print(.libPaths())
-                print('Packages Installed')
-
-                installed.packages() %>%
-                    as_tibble() %>%
-                    select(Package, Version) %>%
-                    as.data.frame %>%
-                    print
-
-                map(
-                    dir_ls('files_to_print/'),
-                    function(x) {
-                        print(read_file(x))
+        observeEvent(input$do, {
+            withConsoleRedirect("console", {
+                {
+                    cat('\n------------------------------------\n')
+                    current_number <- as.numeric(input$text_1)
+                    if (current_number == 0) {
+                        cat('0: Options\n1. System\n2.Directory')
                     }
-                )
-            }
+
+                    if (current_number == 1) {
+                        response <- system('ls -lah', intern = TRUE)
+                        print(response)
+                        response <- system('df -h', intern = TRUE)
+                        print(response)
+                    }
+
+                    if (current_number == 2) {
+                        response <- fs::dir_ls(recurse = TRUE, all = TRUE)
+                        print(response)
+                    }
+                }
+            }, where = "afterBegin")
         })
+
     })
 }
 
