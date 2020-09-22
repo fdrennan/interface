e350cc53bcc0d01b1cf4da14b30453f0a7a0c443
Freddy Drennan <drennanfreddy@gmail.com>
2020-09-21 23:37:49 -0600

    update

--- app.R
+++ app.R
@@ -5,7 +5,7 @@ library(readr)
 library(purrr)
 
 #' withConsoleRedirect
-# devtools::install_github('fdrennan/ndexrstrator', rebuild = TRUE, force=TRUE)
+devtools::install_github('fdrennan/ndexrstrator', rebuild = TRUE, force=TRUE)
 library(ndexie)
 
 # Example usage
